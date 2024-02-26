60869511d163e4d9a47549565bd2e85d07421084
TakumaKouyama <granger33417@outlook.jp>
2024-02-26 18:49:58 +0900

    test

---
+++ sample1/src/sample1/Main.java
@@ -0,0 +1,76 @@
+package sample1;
+
+public class Main {
+	public static void main(String[] args) {
+//		Scanner scr = new Scanner(System.in);
+//		String name = scr.nextLine();
+		
+		int a = 3;
+		int b = a += 5;
+		System.out.println(a + b);
+		
+//		if(name.length() >= 10) {
+//			System.out.println("名前を10文字以内にしてください");
+//		} else if (name.isEmpty() || Objects.isNull(name)) { 
+//			System.out.println("名前を入力してください");
+//		} else {
+//			System.out.println(name +"を登録しました");
+//		}
+		
+		
+		
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		// カリキュラムA
+//		int num = 20;
+//		int num1 = 23;
+//		System.out.println("ハローJAVA" + (num + num1));
+//		
+//		String name = "山田太郎";
+//		int age = 18;
+//		double height = 170.5;
+//		double height2 = height/100;
+//		double weight = 62.2;
+//		String food = "寿司";
+//		double BMI =  weight / height2 / height2;
+//		double BMIans = (double)Math.round(BMI *10)/10;
+//		int fuctAge = age >= 25 ? 1 : 0;
+//		int fuctAge2 = age == 25 || height >=160 ? 1 : 0;
+//		
+//		System.out.println("初めまして" + name + "です");
+//		System.out.println("年齢は" + age + "です");
+//		System.out.println("身長は" + height + "です");
+//		System.out.println("体重は"+ weight + "です");
+//		System.out.println("好きな食べ物は" + food + "です");
+//		System.out.println("BMIは"+ BMIans + "です");
+//		
+//		switch(fuctAge2) {
+//		case 1:
+//			System.out.println("ture");
+//			break;
+//		case 0:
+//			System.out.println("false");
+//			break;
+//		}
+		
+	}
+}
+
